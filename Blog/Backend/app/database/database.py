8da5226885816055441b4abf58c3fd31e56d3e87
ManishMopuri23 <maneesh@stch.com>
2026-06-22 23:54:06 +0530

    fixed some db issues

--- Blog/Backend/app/database/database.py
+++ Blog/Backend/app/database/database.py
@@ -9,9 +9,11 @@ load_dotenv()
 
 DATABASE_URL=os.getenv("DATABASE_URL")
 
-engine=create_engine(
+
+engine = create_engine(
     DATABASE_URL,
-    echo=True
+    pool_pre_ping=True,
+    pool_recycle=300
 )
 
 
